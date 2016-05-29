94594a8394463ea6a8a34e84e93c202eff2cc5ca
Ewelina Fijałkowska <lilka@windowslive.com>
2016-05-29 12:57:17 +0200

    Dodawanie restauracji i listowanie

--- RestaurantApp/RestaurantApp/Program.cs
+++ RestaurantApp/RestaurantApp/Program.cs
@@ -12,7 +12,35 @@ namespace RestaurantApp
 
         static void Main(string[] args)
         {
+            var container = new RestaurantApp.Default.Container(new Uri(RestaurantUri));
+            Console.WriteLine("Dodawanie restuaracji");
+            container.AddToRestaurants(new RestaurantService.Models.Restaurant() { Id = 1, Name = "Pod grusza", Address = "Krakow", Description = "Wege" });
+            var serviceResponse = container.SaveChanges();
 
+            while (true)
+            {
+                Console.WriteLine("Click 'r' to display all restaurants.");
+                var key = Console.ReadKey();
+                Console.ReadLine();
+                if (key.Key == ConsoleKey.R)
+                {
+                    DisplayRestaurants();
+                }
+                else
+                    continue;
+              
+            }
+        }
+
+        public static void DisplayRestaurants()
+        {
+            var container = new RestaurantApp.Default.Container(new Uri(RestaurantUri));
+            Console.WriteLine("All restaurants");
+            foreach (var restaurant in container.Restaurants)
+            {
+                Console.WriteLine("{0} : {1} : {2} : {3}", restaurant.Id, restaurant.Name, restaurant.Address, restaurant.Description);
+            }
+            Console.WriteLine();
         }
     }
 }
