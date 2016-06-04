144e958b1331eaaf371135b4b1c8f06d51e69a3d
dominik.juraszek <djurasze@student.agh.edu.pl>
2016-06-04 17:19:45 +0200

    TokenService updated

--- TokenService/TokenService/WcfService/ITokenService.cs
+++ TokenService/TokenService/WcfService/ITokenService.cs
@@ -8,14 +8,11 @@ using System.Text;
 
 namespace TokenService.WcfService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface ITokenService
     {
-
         [OperationContract]
         TokenObject createTokenForUser(int userId);
-
         [OperationContract]
         TokenObject findUserToken(string token);
     }
