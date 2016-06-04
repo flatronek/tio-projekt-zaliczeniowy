64ce2337da18ec0db528bc1f7fb81a09a438d3b1
dominik.juraszek <djurasze@student.agh.edu.pl>
2016-06-04 17:26:42 +0200

    token service added

---
+++ TokenService/TokenService/DAL/SqlRepository/TokenInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Autofac.Integration.Wcf;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TokenService.WcfService;
+
+namespace TokenService.DAL
+{
+    public class TokenInitializer : DropCreateDatabaseIfModelChanges<TokenContext>
+    {
+        protected override void Seed(TokenContext context)
+        {
+            var tokens = new List<TokenObject>();
+
+            context.Tokens.AddRange(tokens);
+            context.SaveChanges();
+           
+        }
+    }
+}
